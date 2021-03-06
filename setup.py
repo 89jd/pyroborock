42f66ef664f05128489e37a1b55e808482552f4f
redphx <redphx@codekiem.com>
2021-03-06 09:54:40 +0700

    Use tinytuya module (Python) instead of tuyapi (NodeJS)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='pyroborock',
     version='1.0.9',
     packages=['pyroborock'],
-    install_requires=['pytuyapi-ipc==1.0.3', 'python-miio==0.5.3', 'pycryptodome==3.9.8'],
+    install_requires=['tinytuya==v1.2.3', 'python-miio==0.5.3', 'pycryptodome==3.9.8'],
     description='Communicate with roborock over tuya protocol',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
